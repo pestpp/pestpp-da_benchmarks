4d656fe969c3209e0db525911a585c6f53a48636
Matthew Knowling <matthew.knowling@adelaide.edu.au>
2021-01-04 14:36:30 +1030

    base pst interface dev

--- da_tests.py
+++ da_tests.py
@@ -454,124 +454,47 @@ def da_mf6_freyberg_smoother_test():
     assert os.path.exists(phi_file),phi_file
 
 
-def L96(homegrown=True):
-    N = 40  # dims
-    F = 2.0  # forcing
-    h = 0.05  # dt
-    tt = 1000  # total time
+def build_lorenz_pst():
+    t_d = os.path.join("lorenz96", "template_seq")
+    if os.path.exists(t_d):
+        shutil.rmtree(t_d)
+    shutil.copytree(os.path.join("lorenz96", "template"), t_d)
+    os.chdir(os.path.join(t_d))
 
-    # run
-    x = run_L96(N=N, F=F, h=h, tt=tt, homegrown=homegrown)
+    # run forward check
+    pyemu.os_utils.run("python rk4_lorenz96.py")
 
-    # process results
-    #plot_hovmoeller(x, h=h, F=F, tt=tt, homegrown=homegrown)
-    #plot_time_series(x, tt=tt, F=F, homegrown=homegrown)
-    #plot_profiles(x, tt=tt, F=F, homegrown=homegrown)
+    out_csv = "l96_out.csv"
+    odf = pyemu.pst_utils.csv_to_ins_file(os.path.join(out_csv))# only_cols=cols)
+    ins_file = out_csv + ".ins"
 
+    in_csv = "l96_in.csv"
+    F = pd.read_csv(os.path.join(in_csv))['F'][0]
+    tpl_file = in_csv + ".tpl"
 
-def run_L96(N, F, h, tt, homegrown, random_initial=False):
-    '''
-    homegrown : True means to use homegrown fourth-order Runge-Kutta solution;
-        False means to use scipy's odeint (which uses RK4)
-    random_initial : True means to initialize Lorenz state vector randomly;
-        False means to initialize as uniform with some perturbation.
-    '''
+    pst = pyemu.helpers.pst_from_io_files(tpl_file, in_csv, ins_file, out_csv)
 
+    obs = pst.observation_data
+    obs.loc[odf.index, "obsval"] = odf.obsval
 
-    np.random.seed(123)
+    par = pst.parameter_data
+    par.loc["forcing", "parval1"] = F
+    par.loc["forcing", "parlbnd"] = 0.0
+    par.loc["forcing", "parubnd"] = 9.0
 
-    t = np.arange(0.0, tt * h, h)
-    x = np.zeros(shape=(t.shape[0], N))
-    if random_initial:
-        x[0, :] = np.random.rand(N)
-    else:
-        x[0, :] = F * np.ones(N)
-        x[0, 19] += 0.01  # add perturbation
+    pst.control_data.noptmax = 1
+    pst.model_command = "python forward_run.py"
 
-    with open(os.path.join('l96_out.csv'), 'w') as f:
-        f.write(",")
-        for col in range(N):
-            f.write("x_{},".format(col))
-    if homegrown:
-        for ti in range(1, x.shape[0]):  # t:
-            x[ti, :] = rK4(xt=x[ti - 1], h=h, N=N, F=F)
-            with open(os.path.join('l96_out.csv'), 'a') as f:
-                f.write("\n{},".format(ti - 1))
-                [f.write("{},".format(a)) for a in x[ti, :]]
-    else:
-        raise Exception("scipy is not a dependency; just for initial testing purposes")
-        #from scipy.integrate import odeint
-        #x = odeint(lorenz96, x[0], t, args=(N, F))
+    with open(os.path.join("forward_run.py"), 'w') as f:
+        f.write("import rk4_lorenz96\n")
+        f.write("rk4_lorenz96.L96()\n")
 
-    return x
+    pst.write(pst.filename)
 
-def rK4(xt, h, N, F):
-    xt = xt.copy()
-    k1 = h * lorenz96(xt, h, N, F)
-    k2 = h * lorenz96(xt + k1 / 2, h, N, F)
-    k3 = h * lorenz96(xt + k2 / 2, h, N, F)
-    k4 = h * lorenz96(xt + k3, h, N, F)
-    xt += 1 / 6 * (k1 + 2 * (k2 + k3) + k4)
+    #do pestpp test run here
 
-    return xt
+    os.chdir(os.path.join("..", ".."))
 
-def lorenz96(x, t, N, F):
-    # The Lorenz 1996 model (Lorenz E., 1996. ``Predictability: a problem partly solved'')
-
-    dxdt = np.zeros(N)
-    # edges first
-    dxdt[0] = (x[1] - x[N-2]) * x[N-1] - x[0]
-    dxdt[1] = (x[2] - x[N-1]) * x[0] - x[1]
-    dxdt[N-1] = (x[0] - x[N-3]) * x[N-2] - x[N-1]
-    for i in range(2, N-1):  # now the rest
-        dxdt[i] = (x[i+1] - x[i-2]) * x[i-1] - x[i]
-    dxdt = dxdt + F
-
-    # Alternatively can use np.roll
-    #dxdt = (-np.roll(x, 1) * (np.roll(x, 2) - np.roll(x, -1)) - x + F)
-
-    return dxdt
-
-def plot_hovmoeller(x, h, F, tt, homegrown, plot_all_times=True):
-    fig1, ax2 = plt.subplots()
-    if plot_all_times:
-        assert tt == x.shape[0]
-        t_range = np.arange(0, x.shape[0] * h, h)
-        xxx, yyy = np.meshgrid(np.arange(x.shape[1]), t_range)
-        CS = ax2.contourf(xxx, yyy, x[:])
-    else:
-        last_few = 200
-        t_range = np.arange((x.shape[0] - last_few) * h, x.shape[0] * h, h)
-        xxx, yyy = np.meshgrid(np.arange(x.shape[1]), t_range)
-        CS = ax2.contourf(xxx, yyy, x[-t_range.shape[0]:])
-    c = plt.colorbar(CS)
-    ax2.set_xlabel("equidistant sites along latitude circle", fontsize=12)
-    if plot_all_times:
-        ax2.set_ylabel("time", fontsize=12)
-    else:
-        ax2.set_ylabel("time step index", fontsize=12)
-    ax2.set_title("$F$ = {}".format(F), fontsize=12)
-    #plt.savefig("l96_hovmoeller_F{0}_homegrown{1}.pdf".format(F, homegrown))
-
-def plot_time_series(x, tt, F, homegrown):
-    fig, ax = plt.subplots()
-    start_time, end_time = tt - 100, tt
-    start_loc, end_loc = 5, 8
-    lines = ax.plot(np.arange(start_time, end_time),
-                    x[start_time:end_time, start_loc:end_loc])
-    ax.legend(lines, list(np.arange(start_loc, end_loc)), loc='upper right')
-    ax.set_ylabel("$x_{0},..,x_{1}$".format(start_loc, end_loc - 1))
-    ax.set_xlabel("time step index")
-    #plt.savefig("lorenz_ts_F{0}_homegrown{1}.pdf".format(F, homegrown))
-
-def plot_profiles(x, tt, F, homegrown):
-    fig, ax = plt.subplots()
-    time = tt - 1  #start_time, end_time = 4000, 4100
-    lines = ax.plot(np.arange(x.shape[1]), x[time, :])
-    #ax.legend(lines, list(np.arange(1)), loc='upper right')
-    ax.set_ylabel("$x$ at time step {0}".format(time))
-    ax.set_xlabel("site along lat circle")
-    #plt.savefig("lorenz_profile_F{0}_homegrown{1}.pdf".format(F, homegrown))
 
 if __name__ == "__main__":
     
